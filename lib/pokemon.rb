55f303bd0340b90ea6715583a4afec1f70dcd7e9
Shaundra <9601515+Shaundra@users.noreply.github.com>
2019-02-06 20:27:50 -0800

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,41 @@
 class Pokemon
+  attr_reader :id, :name, :type, :db, :hp
+
+  def initialize(id:, name:, type:, db:, hp: nil)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+    @hp = hp
+  end
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+  end
+
+  def self.find(id, db)
+    results = db.execute("SELECT * FROM pokemon WHERE id = ?", id)[0]
+    Pokemon.new(id: results[0], name: results[1], type: results[2], hp: results[3], db: db)
+  end
+
+  def alter_hp(new_hp, db)
+    sql = <<-SQL
+      UPDATE pokemon
+      SET hp = ?
+      WHERE id = ?
+    SQL
+
+    db.execute(sql, new_hp, self.id)
+  end
 end
+
+# [1, "Pikachu", "electric"]
+# @db = SQLite3::Database.new(':memory:')
+# db.execute("CREATE TABLE IF NOT EXISTS pokemon
+# (
+#     id INTEGER PRIMARY KEY
+#   , name TEXT
+#   , type TEXT
+#   , hp INTEGER
+# );
+# ")
